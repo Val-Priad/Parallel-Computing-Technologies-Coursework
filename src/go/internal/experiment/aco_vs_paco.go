aae213da8672acbf3a799ed708a64c2f423a4a19
Val-Priad <valeriipriadchenko@gmail.com>
2026-04-30 22:58:03 +0200

    refactor

--- src/go/internal/experiment/aco_vs_paco.go
+++ src/go/internal/experiment/aco_vs_paco.go
@@ -72,7 +72,7 @@ func RunACOvsPACO() {
 			acoTotalCost += acoSolution.Cost
 			acoTotalDuration += acoSolution.DurationMS
 
-			pacoCfg := solver.PACOConfig{BaseConfig: solver.DefaultACOConfig()}
+			pacoCfg := solver.PACOConfig{BaseConfig: solver.DefaultACOConfig(), NumWorkers: 16}
 			pacoCfg.BaseConfig.Seed = seed
 			pacoSolution := solver.SolvePACO(instance, pacoCfg)
 			pacoTotalCost += pacoSolution.Cost
--- src/go/internal/experiment/paco_tuning.go
+++ src/go/internal/experiment/paco_tuning.go
@@ -49,7 +49,7 @@ func RunPACOProcessTuning() {
 
 	baseCfg := solver.DefaultACOConfig()
 
-	for workers := 1; workers <= 50; workers++ {
+	for workers := 1; workers <= 40; workers++ {
 		fmt.Printf("workers=%d...\n", workers)
 
 		runCfg := solver.PACOConfig{
